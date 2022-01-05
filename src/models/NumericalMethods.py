f0fb33a3bc67b6c126d36f06313bfa0913a903dc
AlbertDeHoz <alberto.entreprises@gmail.com>
2022-01-05 09:45:47 -0500

    adding print method to Bisection

--- src/models/NumericalMethods.py
+++ src/models/NumericalMethods.py
@@ -11,19 +11,36 @@ class Bisection(NumericalMethods):
         NumericalMethods.__init__(self,function,iter_max)
         self.xl = xl
         self.xu = xu
+        self.set_xr()
 
-    def evaluate_once(self):
+    def set_xr(self):
         f = self.function
         if f(self.xl) * f(self.xu) >= 0 :
             raise Exception("f(xl) * f(xu) must be lower than 0");
-        xr = ( self.xl + self.xu )/2
+        self.xr = ( self.xl + self.xu )/2
         
-        if f(self.xl) * f(xr) < 0:
-            return Bisection( f,self.iter_max-1, self.xl, xr)
-        elif f(self.xl) * f(xr) > 0:
-            return Bisection( f,self.iter_max-1, xr, self.xu )
+    def evaluate_once(self):
+        f = self.function
+        if f(self.xl) * f(self.xr) < 0:
+            return Bisection( f,self.iter_max-1, self.xl, self.xr)
+        elif f(self.xl) * f(self.xr) > 0:
+            return Bisection( f,self.iter_max-1, self.xr, self.xu )
 
-        return 0
+        return None
+
+    def __evaluate(self):
+        iterations = []
+        current = self
+        while current.iter_max > 0:
+            current = current.evaluate_once()
+            iterations.append(current)
+        return [*iterations]
+    
+    def print(self,**config):
+        iterations = self.__evaluate()
+        for iter in iterations:
+            print(iter)
+
 
     def __str__(self):
-        return f"[ xl={self.xl},  xu={self.xu}, iter={self.iter_max} ]"
+        return f"[ xl={self.xl}, xu={self.xu}, xr={self.xr}, iter={self.iter_max} ]"
--- src/models/main.py
+++ src/models/main.py
@@ -3,12 +3,15 @@ from NumericalMethods import NumericalMethods
 function = NumericalMethods(lambda x : 2*x-2, 3)
 
 bisection = function.bisection(-30, 34)
-b_1 = bisection.evaluate_once()
-b_2 = b_1.evaluate_once()
-b_3 = b_2.evaluate_once()
 
-print(bisection)
-print(b_1)
-print(b_2)
-print(b_3)
+bisection.print()
+# b_1 = bisection.evaluate_once()
+# print(b_1)
+# b_2 = b_1.evaluate_once()
+# b_3 = b_2.evaluate_once()
+# 
+# print(bisection)
+# print(b_1)
+# print(b_2)
+# print(b_3)
 # bisection.evaluate_once()
